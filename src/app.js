282678d1eb6a28b8f677d84f58aa1a0af179ac58
RowDog <mrdiamonddirt@gmail.com>
2022-11-07 14:47:19 +0000

    📋 Added commented task

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+// Task:
+// • Build an improved version of the movie app.
+// • Use the yargs commands to collect targeted
+// data from each user response.
+// • Define functions in a separate file and import to
+// app.js.
+// • Store optional data on movies.
+// • Console log user input at app termination.
+// Stretch goal - Add multiple movies.
+
 const yargs = require('yargs')
 
 const input = yargs.argv;
